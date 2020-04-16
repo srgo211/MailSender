f8ecb8ee9586b36b61f333cfeb73a235c9cf837b
Сергей Новиков <srgo211@mail.ru>
2020-04-16 03:33:49 +0300

    Успешная отправка смс в TestWPFramework

--- Tests/TestWpfFramework/MainWindow.xaml.cs
+++ Tests/TestWpfFramework/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,6 +29,57 @@ namespace TestWpfFramework
         private void sendToEmail_Click(object sender, RoutedEventArgs e)
         {
 
+
+            // отправитель - он же логин от почты
+            string from = userName.Text;
+            //пароль
+            string passToEmail = password.Password;
+
+            // кому отправляем
+            string to = "evgeniyzhuravlyov2000018dwh@rambler.ua";
+
+
+            try
+            {
+                // создаем объект сообщения
+                using (MailMessage msg = new MailMessage(from, to))
+                {
+                    msg.Subject = "Тема сообщения от" + DateTime.Now;
+                    msg.Body = "Текст сообщения: " + DateTime.Now;
+                    msg.IsBodyHtml = false;
+
+                    // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                    using (SmtpClient smtp = new SmtpClient())
+                    {
+                        /*
+                         * настройки для рамблер почты
+                         * если используем шифрование (рекомендуется) то порт ставим 465
+                         * p.s. свежереги сначала не поддерживует шифрование - поэтому порт 25 или 465
+                         */
+                        smtp.Host = "smtp.rambler.ru";
+                        smtp.Port = 25; //порт
+                        smtp.EnableSsl = false; // не используем шифрование
+                        smtp.Credentials = new System.Net.NetworkCredential(from, passToEmail); //проверка учетной записи логин и пароль от почты
+
+                        smtp.Send(msg);
+
+                        MessageBox.Show("Почта отправлена", "Успешно!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("ОШИБКА", error.Message, MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
         }
-    }
-}
+
+
+
+
+
+
+
+
+    }//public partial class MainWindow
+}//namespace TestWpfFramework
