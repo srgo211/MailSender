a0452fe4af4d1d7cb13a7dbd4e16c525b65e7bcb
Сергей Новиков <srgo211@mail.ru>
2020-04-17 01:14:29 +0300

    Сущности отправителя и сервера почты [LibMailSender]

---
+++ LibMailSender/LibMailSender/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LibMailSender
+{
+    public class Class1
+    {
+    }
+}
---
+++ LibMailSender/LibMailSender/Entities/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibMailSender.Entities
+{
+    public class Server
+    {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Порт
+        /// </summary>
+        public int Port { get; set; }
+        /// <summary>
+        /// Использовать защищенное соединение
+        /// </summary>
+        public bool UseSSL { get; set; }
+
+
+
+    }
+}
