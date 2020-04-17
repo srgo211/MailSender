d674e507a0097bbf31eea5464674c644d626b6b3
Сергей Новиков <srgo211@mail.ru>
2020-04-18 02:15:38 +0300

    Сделал тестовую привязку некоторых кнопок [MailSender]

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -18,9 +18,11 @@ namespace MailSender
 
         private void btnSendClick(object sender, RoutedEventArgs e)
         {
-            //извлекаем данные получателей писем
+            //извлекаем данные
             var recipient = RecipientsList.SelectedItem as Recipient;
+            //получаем данные отправителя
             var senders = SendersList.SelectedItem as Sender;
+            //получаем данные сервера
             var server = ServersList.SelectedItem as Server;
 
             //проверка
@@ -31,6 +33,27 @@ namespace MailSender
             emailSend.Send(MailHeader.Text,MailBody.Text,senders.AddressToEmail,recipient.AddressToEmail);
         
         
+        }
+        /// <summary>
+        /// Клик Редактировать отправителя
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEditClick(object sender, RoutedEventArgs e)
+        {
+            //получаем данные отправителя
+            var senders = SendersList.SelectedItem as Sender;
+            if (senders is null) return;
+
+            var dialog = new SenderEditor(senders);
+
+            if (dialog.ShowDialog() !=true) return;
+
+            senders.Name = dialog.Name;
+            senders.AddressToEmail = dialog.AdresValue;
+
+
+
         }
     }   
 }
---
+++ MailSender/SenderEditor.xaml.cs
@@ -0,0 +1,45 @@
+﻿using LibMailSender.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MailSender
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для SenderEditor.xaml
+    /// </summary>
+    public partial class SenderEditor
+    {
+        public string NameValue { get => NameEditSender.Text; set => NameEditSender.Text = value;}
+        public string AdresValue { get => AdressEditSender.Text; set => AdressEditSender.Text = value; }
+
+
+        public SenderEditor(Sender Sender )
+        {
+            InitializeComponent();
+            NameValue = Sender.Name;
+            AdresValue = Sender.AddressToEmail;
+        }
+
+        private void btnSenderEditOkClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void btnSenderEditCanselClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
