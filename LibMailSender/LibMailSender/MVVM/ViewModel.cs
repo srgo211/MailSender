d9f30f84e2c12fb48d4b461f3da1126b17e2de30
Сергей Новиков <srgo211@mail.ru>
2020-04-18 05:16:00 +0300

    Начало реализации MVVM

---
+++ LibMailSender/LibMailSender/MVVM/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LibMailSender.MVVM
+{
+    /// <summary>
+    /// Модель представления
+    /// </summary>
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+
+        }
+
+    }
+}
