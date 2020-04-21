1d0af4da4766f696232efaba8b2836fe9e6be3cb
Сергей Новиков <srgo211@mail.ru>
2020-04-21 06:34:42 +0300

    Переработка сервиса хранения данных получателей письма

---
+++ LibMailSender/LibMailSender/Modules/InMemory/DataStoreInMemory.cs
@@ -0,0 +1,63 @@
+﻿using LibMailSender.Entities.Base;
+using LibMailSender.Modules.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibMailSender.Modules.InMemory
+{
+    public abstract class DataStoreInMemory<T> : IDataStore<T> where T : BaseEntity
+    {
+        private readonly List<T> _Items;
+
+        protected DataStoreInMemory(List<T> Items = null) => _Items = Items ?? new List<T>();
+
+        /// <summary>
+        ///  Интерфейс для хранения инфо в памяти
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> GetAll() => _Items;
+
+        /// <summary>
+        /// Ворачивает найденный элемент по ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T GetById(int id) => _Items.FirstOrDefault(item => item.Id == id);
+
+        public int Create(T item)
+        {
+            //Если в нашем списке элемент содержится  - то делать ничего не будем
+            if (_Items.Contains(item)) return item.Id;
+
+            //если в списке Получателей 0 записей то присваиваем 1 или след. запись
+            item.Id = _Items.Count == 0 ? 1
+                            : _Items.Max(r => r.Id) + 1;
+            //Добавляем получателя в список
+            _Items.Add(item);
+            return item.Id;
+
+            /* TODO 
+             * Если используем БД - то прописываем код для добавления в БД
+             */
+        }
+
+        public abstract void Edit(int id, T item);
+
+
+
+        public T Remove(int id)
+        {
+            var item = GetById(id);
+            if (item != null)
+                _Items.Remove(item);
+            return item;
+        }
+
+        public void SaveChanges()
+        {
+            System.Diagnostics.Debug.WriteLine("Сохранение изменений в хранилище {0}.", typeof(T));
+        }
+    }
+}
--- LibMailSender/LibMailSender/Modules/InMemory/RecipientsStoreInMemory.cs
+++ LibMailSender/LibMailSender/Modules/InMemory/RecipientsStoreInMemory.cs
@@ -5,57 +5,21 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using LibMailSender.Modules.InMemory;
 
 namespace LibMailSender.Modules
 {
-    public class RecipientsStoreInMemory : IRecipientsStore
+    public class RecipientsStoreInMemory : DataStoreInMemory<Recipient>, IRecipientsStore
     {
 
-
-        /// <summary>
-        /// Интерфейс для хранения инфо в памяти
-        /// </summary>
-        /// <returns></returns>
-        public IEnumerable<Recipient> GetAll() => TestData.ListRecipients;
-
-
-        /// <summary>
-        /// Ворачивает найденный элемент по ID
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public Recipient GetById(int id) => TestData.ListRecipients.FirstOrDefault(d => d.Id == id);
-
-        /// <summary>
-        ////Создать получателя
-        /// </summary>
-        /// <param name="Recipient"></param>
-        /// <returns></returns>
-        public int Create(Recipient Recipient)
-        {
-            //Если в нашем списке элемент содержится  - то делать ничего не будем
-            if (TestData.ListRecipients.Contains(Recipient)) return Recipient.Id;
-            
-            //если в списке Получателей 0 записей то присваиваем 1 или след. запись
-            Recipient.Id = TestData.ListRecipients.Count == 0 ? 1
-                            : TestData.ListRecipients.Max(r=> r.Id) + 1;
-            //Добавляем получателя в список
-            TestData.ListRecipients.Add(Recipient);
-            return Recipient.Id;
-
-            /* TODO 
-             * Если используем БД - то прописываем код для добавления в БД
-             */
-        }
-
-
+        public RecipientsStoreInMemory() : base(TestData.ListRecipients) { }
 
         /// <summary>
         /// Правка Получателей
         /// </summary>
         /// <param name="id">Идентификатор получателя</param>
         /// <param name="recipient">информацию которую нужно править</param>
-        public void Edit(int id, Recipient recipient)
+        public override void Edit(int id, Recipient recipient)
         {
             /*т.к. у нас хранится в памяти то ничего не делаем,
              * но сейчас пример как можно это реализовать
@@ -73,31 +37,9 @@ namespace LibMailSender.Modules
 
         }
 
-        /// <summary>
-        /// Удалить получателя
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public Recipient Remove(int id)
-        {
-            var db_recipient = GetById(id);
-            if (db_recipient != null)
-                TestData.ListRecipients.Remove(db_recipient);
-            return db_recipient;
+     
 
-        }
-
-        /// <summary>
-        /// Сохранение информации
-        /// </summary>
-        public void SaveChanges()
-        {
-            /*т.к. у нас хранится в памяти то ничего не делаем
-             * если хранили в БД
-             * то внести измения в БД
-             * */
-            System.Diagnostics.Debug.WriteLine("Сохранение изменений в хранилище получателей писем");
-        }
+  
 
     }
 }
