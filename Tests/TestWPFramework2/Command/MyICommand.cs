e6ada9a958cf1d6b82b7fff828022eb7f2542f1d
Сергей Новиков <srgo211@mail.ru>
2020-04-23 20:44:58 +0300

    Сделал отбражение списков и таблиц + добавление в список [TestWPFramework2]

---
+++ Tests/TestWPFramework2/Command/MyICommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestWPFramework2.Command
+{
+    public delegate void CustomVarsDelegate(Dictionary<string, object> vars);
+
+    public class MyICommand : ICommand
+    {
+
+        #region ICommand - Реализация интерфейса
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (_TargetCanExecuteMethod != null)
+            {
+                return _TargetCanExecuteMethod();
+            }
+            else if (_TargetExecuteMethod != null)
+                return true;
+            return false;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            if (_TargetExecuteMethod != null)
+            {
+                _TargetExecuteMethod();
+            }
+        }
+
+        #endregion ICommand Members
+
+        private Func<bool> _TargetCanExecuteMethod;
+        private Action _TargetExecuteMethod;
+
+
+        public MyICommand(Action executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public MyICommand(Action executeMethod, Func<bool> canExecuteMethod)
+            : this(executeMethod)
+        {
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        [field: NonSerializedAttribute()]
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+       
+    }
+}
--- Tests/TestWPFramework2/ViewModel/MainViewModel.cs
+++ Tests/TestWPFramework2/ViewModel/MainViewModel.cs
@@ -7,39 +7,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using TestWPFramework2.MVVM;
+using TestWPFramework2.Command;
 
 namespace TestWPFramework2.ViewModel
 {
     internal class MainViewModel : ViewModelBase
     {
-        public ICommand CreateRecipientCommand { get; }
 
-     
-        #region Обработка текста Text
-        private string _Text = "Любой текст";
 
-        public string Text
+        public MainViewModel()
         {
-            get => _Text;
+            MyList = new ObservableCollection<MyListClass>(GetDataStudent());
+            SaveStudentCMD = new MyICommand(SaveStudent);
+            DeleteStudentCMD = new MyICommand(DeleteStudent);
+        }
+
+
+
+
+
+        public ICommand CreateRecipientCommand { get; }
+        public ICommand SaveStudentCMD { get; set; }
+        public ICommand DeleteStudentCMD { get; set; }
+
+        #region Обработка текста Text
+        private string _Name = "Любой текст";
+
+        public string Name
+        {
+            get => _Name;
             //set => Set(ref _Text, value);
 
             set
             {
-                if (Set(ref _Text, value))
+                if (Set(ref _Name, value))
                     OnPropertyChanged(nameof(TextLight));
-                    OnPropertyChanged(nameof(MyList));
+                    //OnPropertyChanged(nameof(MyList));
             }
 
-    }
-    #endregion
+        }
+        #endregion
 
 
 
-    public string TextLight => _Text.Length.ToString();
+        public string TextLight
+        {
+            get
+            {
+                if (Name != null) return Name.Length.ToString();
+                return "0";
+            }
+        }
 
-
-
-
+        #region [Collection]
         private ObservableCollection<MyListClass> _MyList;
 
         public ObservableCollection<MyListClass> MyList
@@ -47,13 +67,95 @@ namespace TestWPFramework2.ViewModel
             get => _MyList;
             private set => Set(ref _MyList, value);
         }
+        #endregion
+
+
+        #region [Variable]
+        private string _id;
+        public string id { get { return _id; } set { _id = value; OnPropertyChanged(); } }
+
+        //private string _Name;
+       // public string Name { get { return _Name; } set { _Name = value; OnPropertyChanged(); } }
+        #endregion
+
+
+        #region [Command]
+        public ICommand ShowDialogAddStudentCMD { get; set; }
+
+        #endregion
+
+
+        public List<MyListClass> GetDataStudent()
+        {
+            List<MyListClass> studentsLits = new List<MyListClass>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                MyListClass student = new MyListClass();
+                student.id = i.ToString();
+                student.name = "Name-" + i;
+                
+                studentsLits.Add(student);
+            }
+            return studentsLits;
+        }
+
+        private MyListClass _selectedItemStudent;
+        public MyListClass SelectedItemStudent
+        {
+            get
+            {
+                return _selectedItemStudent;
+            }
+            set
+            {
+                _selectedItemStudent = value;
+                OnPropertyChanged();
+                if (_selectedItemStudent != null)
+                {
+                    Name = SelectedItemStudent.name;
+                    id = SelectedItemStudent.id.ToString();
+                   
+                }
+            }
+        }
+
+
+
+
+        public void SaveStudent()
+        {
+           
+                MyListClass student = new MyListClass();
+                student.id = new Random().Next(0,100).ToString();
+                student.name = Name;
+
+                MyList.Add(student);
+
+                id = String.Empty;
+                Name = String.Empty;
+
+                //IsShowdialog = false;
+            
+        }
+
+        public void DeleteStudent()
+        {
+            if (SelectedItemStudent != null)
+            {
+                MyList.Remove(SelectedItemStudent);
+                Name = String.Empty;
+                id = String.Empty;
+
+            }
+        }
 
     }
 
-     public class MyListClass
+    public class MyListClass
     {
 
-        public int id { get; set; }
+        public string id { get; set; }
 
         public string name { get; set; }
 
